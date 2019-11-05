50810df580a39d8daccde96092b929be74bdf7e9
mtai000 <605836858@qq.com>
2019-11-05 20:45:51 +0800

    fix 70

--- 70.爬楼梯.cpp
+++ 70.爬楼梯.cpp
@@ -14,7 +14,7 @@ public:
         if(n==1)return n1;
         if(n==2)return n2;
         int temp = 0;
-        for(int i =2 ; i<= n; i++)
+        for(int i =3 ; i<= n; i++)
         {
             temp = n1 + n2;
             n1 = n2;
