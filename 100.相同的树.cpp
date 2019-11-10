d986746536f02f139e967aa3cc6b94a4aba0a7ea
mtai000 <605836858@qq.com>
2019-11-10 16:46:35 +0800

    100

--- 100.相同的树.cpp
+++ 100.相同的树.cpp
@@ -60,7 +60,13 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
+        if( p == nullptr && q ) return false;
+        if( p && q == nullptr ) return false;
+        if( !p && !q ) return true;
+        if(p->val == q->val) 
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        else
+            return false;
     }
 };
 
