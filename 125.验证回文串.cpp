050266e78d9404344d87c09392917a10ea209fe7
mtai000 <605836858@qq.com>
2019-11-12 13:03:04 +0800

    update 125

--- 125.验证回文串.cpp
+++ 125.验证回文串.cpp
@@ -7,7 +7,36 @@
 // @lc code=start
 class Solution {
 public:
-    bool isPalindrome(string s) {
+    bool isPalindrome(string s)
+    {
+        if(!s.length()) return true;
+        for(int i=0,j=s.length()-1;i<j;)
+        {
+            bool iIsLegal = isLegalChar(s[i]);
+            bool jIsLegal = isLegalChar(s[j]);
+            if(!iIsLegal) ++i;
+            if(!jIsLegal) --j;
+            if(!iIsLegal || !jIsLegal) continue;
+
+            if(s[i] >= 'a') s[i] = (char)(s[i] - 32 );
+            if(s[j] >= 'a') s[j] = (char)(s[j] - 32 );
+            if(s[i] == s[j])
+            {
+                ++i,--j;
+            }
+            else
+            {
+                return false;
+            }     
+        }
+        return true;
+
+    }
+    bool isLegalChar(char c)
+    {
+        return (c >='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='Z');
+    }
+    /*bool isPalindrome(string s) {
         //cout << s.length() << endl;
         //cout << s << endl;
         if(!s.length()) return true;
@@ -18,9 +47,9 @@ public:
             cout << s[i];
             if( (s[i]>='A' && s[i]<='Z') ||
                    (s[i] >= '0' && s[i] <='9'))
-                s2.append(/*len++ ,*/1,s[i]);
+                s2.append(1,s[i]);
             if( s[i]>='a' && s[i]<='z' )
-                s2.append(/*len++ ,*/1,(char)(s[i] - 32));
+                s2.append(1,(char)(s[i] - 32));
             //cout << len << endl;
         }
         cout << s2 << endl;
@@ -33,7 +62,7 @@ public:
                 return false;
         }
         return true;
-    }
+    }*/
 
 
 };
