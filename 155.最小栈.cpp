6e0da9cdcd299e7bb3aae5427884f8bafef6264a
mtai000 <605836858@qq.com>
2019-11-15 14:21:43 +0800

    155

---
+++ 155.最小栈.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=cpp
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    struct LNode{        
+        int val;
+        int min;
+        LNode *next;
+    }; 
+
+    LNode* head = NULL;
+
+    MinStack() { 
+        //head = new LNode();
+    }
+    
+    void push(int x) {
+        LNode *ln = new LNode();
+        ln->val = x;
+        ln->next = head;
+        if(head)
+        {
+            ln->min = min(x,ln->next->min);
+        }
+        else
+        {
+            ln->min = ln->val;
+        }
+        head = ln;
+    }
+    
+    void pop() {
+        LNode *tmp;
+        if( head )
+            tmp = head->next;
+            delete head;
+        head = tmp;
+    }
+    
+    int top() {
+        return head->val;
+    }
+    
+    int getMin() {
+        return head->min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+// @lc code=end
+
