7b6123349bba8b5fd2ddae60eb4a9f20263138e0
mtai000 <605836858@qq.com>
2020-01-12 16:17:59 +0800

    up

---
+++ 263.丑数.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=cpp
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num == 0) return false;
+        int chu[3] = {2,3,5};
+     
+        for(int i = 0 ;i < 3 ; )
+        {
+            if( num % chu[i] == 0)
+            {
+                num /= chu[i];
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (num == 1)
+            return true;
+        else 
+            return false;
+    }
+};
+// @lc code=end
+
