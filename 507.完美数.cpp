ba83c0b18d1dc7df069003c671ec047ef8fd367c
mtai000 <605836858@qq.com>
2020-01-14 19:27:35 +0800

    507

---
+++ 507.完美数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=507 lang=cpp
+ *
+ * [507] 完美数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        
+    }
+};
+// @lc code=end
+
