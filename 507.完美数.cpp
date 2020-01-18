acd32ecb72f2d9b5006f028cb9408a1436684ed6
mtai000 <605836858@qq.com>
2020-01-18 15:31:44 +0800

    up

--- 507.完美数.cpp
+++ 507.完美数.cpp
@@ -8,7 +8,16 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        
+        if(num == 1 ) return false;
+        int sum = 1 ;
+        for(int i = 2 ; i <= sqrt(num) && sum <= num; i++)
+        {
+            if( !(num%i))
+            {
+                sum += i + num/i; 
+            }
+        }
+        return !(num - sum);
     }
 };
 // @lc code=end
